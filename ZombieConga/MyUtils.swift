822836f516e626f06ff507a663e9912ad7b22d91
Valentin Silvera <valentinsilvera@icloud.com>
2020-03-08 00:15:02 +0100

    Smooth turning and stopping

---
+++ ZombieConga/MyUtils.swift
@@ -0,0 +1,9 @@
+//
+//  MathUtilities.swift
+//  ZombieConga
+//
+//  Created by Valentin Silvera on 07/03/2020.
+//  Copyright © 2020 Valentin Silvera. All rights reserved.
+//
+
+import Foundation
